9330a7797139f752ab360692d64ac52cc4fb1cc3
Christopher Su <christophersu9@gmail.com>
2014-10-29 22:28:32 -0700

    moving serverside

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import random
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 
@@ -26,12 +27,19 @@ else:
 # Routes
 # ###################################################################
 
+products = ['alarm clock', 'television', 'microwave', 'keyboard', 'water bottle', 'eBook reader', 'phone', 'chair', 'trash can', 'stopwatch', 'wallet', 'keychain', 'bicycle', 'binder', 'notebook']
+constraints = ['for the visually impaired', 'for the elderly', 'for children', 'for families with children', 'for the hearing impaired', 'for the speech impaired', 'for astronauts']
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', header='Product Prompt', title='Product Prompt', jumbotron='''
         <p class="lead" id="prompt">Click the button below to get a product design prompt!</p>
         <p><a class="btn btn-lg btn-success" id="prompt-btn" href="#" role="button">Acquire Prompt</a></p>
         ''')
+
+@app.route('/random', methods=['GET'])
+def random():
+    return render_template('redirect.html', url=app.config['base_uri'] + '/' + random.choice(products) + '/' + random.choice(constraints))
 
 # ###################################################################
 # Start Flask
